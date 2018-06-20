34776b0527adcdab8d5001ae22e8c90100ec88b9
AHN Jaehong <monsieur.ahn@gmail.com>
2018-06-20 22:44:26 +0900

    Add comments on IntSetArray

--- DevSet/IntSetArray.cpp
+++ DevSet/IntSetArray.cpp
@@ -9,31 +9,7 @@
 class IntSetArray : IntSet {
 private:
 	int n;
-	int *arr;
-	int arr_i;
-
-	bool dupCheck(int target) {
-		/*
-		Duplication check implemented based on binary search which takes o(log n) on average
-		*/
-
-		int low = 0;
-		int mid = 0;
-		int high = n - 1;
-
-		int midVal;
-
-		while (low <= high) {
-			mid = (low + high) >> 1;
-			midVal = arr[mid];
-
-			if (midVal < target) low = mid + 1;
-			else if (midVal > target) high = mid - 1;
-			else return true;
-		}
-
-		return false;
-	}
+	bool *arr;
 
 public:
 	// constructor
@@ -44,18 +20,18 @@ public:
 		/*
 		This code occurs sparsity problem of array resulting in waste of memory
 		but it is necessary to compromise (time complexity and space complexity)
-		By giving up space complexity, it is definitely expected to reduce 
+		By giving up space complexity, it is definitely expected to reduce time complexity
+		and not to sort the array when it comes to inserting or printing values.
 		*/
 
-		arr = new bool[get_maxval()+1];
+		arr = new bool[max_value];
 
 		//initialize arr
-		for (int i = 0; i < max_elements + 1; i++) {
+		for (int i = 0; i < max_value + 1; i++) {
 			arr[i] = false;
 		}
 			
 		n = 0;
-		arr_i = 0;
 	}
 
 	int size() { return n; }
@@ -63,23 +39,18 @@ public:
 	void insert(int num) {
 		/*
 		Checking whether incoming number 'n' is already in array
-		Even though the process takes only the time complexity of o(1),
-		in this way, too much of memory space is wasted
-		
+		This process takes only the time complexity of O(1),
+		*/
 		if (!arr[num]){	
 			arr[num] = true;
 			n++;
-		}
-		*/
-
-		if (!dupCheck(num)) {
-			
 		}
 	}
 
 	void report(int *v) {
 		int j = 0;
-		for (int i = 0; i < get_maxval() + 1; i++)
+		for (int i = 0; i < get_maxval() + 1; i++) {
 			if (arr[i]) v[j++] = i;
+		}
 	}
 };
